50d4c290cb1b435cbc3a690a9d30f8922112ca8d
Lars Wächter <lars97.waechter@gmail.com>
2021-10-23 18:18:18 +0200

    Refactoring

--- README.md
+++ README.md
@@ -1,107 +1,68 @@
 # listing.js
-JavaScript library for working with comma separated lists
 
-[![Build Status](https://travis-ci.com/larswaechter/listing.js.svg?branch=master)](https://travis-ci.com/larswaechter/listing.js)
+A JavaScript library for working with comma separated lists
 
 [![NPM](https://nodei.co/npm/listing.js.png)](https://nodei.co/npm/listing.js/)
 
 ## Introduction:
+
 listing.js is a library that simplifies the work with comma separated lists in JavaScript. Most of the features are similar to those of array methods. Besides commas, other delimiters are supported as well.
 
 ## Installation:
+
 Install via npm
+
 ```
 npm i --save listing.js
 ```
 
 ## Usage:
+
 ```javascript
-const listing = require('listing.js');
+const Listing = require('listing.js');
 
-// ES6: import listing from 'listing.js';
+// ES6: import Listing from 'listing.js';
 
-const myList = new listing('1,2,3,4');
+const myList = new Listing('1,2,3,4');
 myList.append(5);
 
-myList.each(val => {
-  console.log(val);
+myList.each((val) => {
+	console.log(val);
 });
 ```
 
 ## Valid delimiters and lists:
-listing.js allows three different delimiters:
+
+listing.js allows three different delimiters. In this case, each number is a single list item.
+
 ```javascript
-const listing = require('listing.js');
+const Listing = require('listing.js');
 
-// ,
-const myList_1 = new listing('1,2,3,4');
-
-// ;
-const myList_2 = new listing('1;2;3;4');
-
-// :
-const myList_3 = new listing('1:2:3:4');
+const myList_1 = new Listing('1,2,3,4'); // ,
+const myList_2 = new Listing('1;2;3;4'); // ;
+const myList_3 = new Listing('1:2:3:4'); // :
 ```
 
-In this case, each single number is a list element.
+You can also use strings instead of numbers as items:
 
-You can also use strings instead of numbers as elements:
 ```javascript
-const listing = require('listing.js');
+const Listing = require('listing.js');
 
-const myList = new listing('hello,I,am,Lars');
+const myList = new Listing('hello,I,am,John');
 ```
 
-The list elements are accessible via the ```.list``` property.
+The list items are accessible via the `.list` property.
 Example:
+
 ```javascript
 const listing = require('listing.js');
 
 const myList = new listing('1,2,3');
-
-// Expected output: 1,2,3
-console.log(myList.list)
+console.log(myList.list); // Expected output: 1,2,3
 ```
 
 ## Documentation
 
-### Properties / Getters
-- `list`: Return list elements
-- `length`: Return number of list elements
-- `delimmiter`: Return list delimiter
-
-### Static methods
-- `toList(array, delimiter)`: Create new Listing instance from array
-
-### Methods
-- `append(value)`: Append new element to list
-- `clear()`: Clear list
-- `concat(listing)`: Concat two lists into one
-- `contains(substring)`: Check if a list element contains a certain substring and return its position (case-sensitive)
-- `containsNoCase(substring)`: Check if a list element contains a certain substring and return its position (case-insensitive)
-- `deleteAt(pos)`: Delete a list element at given position
-- `each(cb)`: Loop list and return each single value and its index in given callback
-- `filter(fn)`: Return new listing instance with all list elements that pass a test
-- `find(value)`: Check if list includes given value (case-sensitive)
-- `findNoCase(value)`: Check if list includes given value (case-insensitive)
-- `first()`: Get first list element
-- `getAt(pos)`: Get list element at given position
-- `getDuplicates()`: Return new listing instance of duplicated list elements
-- `indexOf(value)`: Get position in list of given value
-- `insertAt(pos, value)`: Insert element at given position
-- `last()`: Get last list element
-- `map(fn)`: Return new listing instance with the results of calling a function for every list element
-- `prepend(value)`: Prepend element to list
-- `qualify(value)`: Add element at start and end of list
-- `removeDuplicates()`: Remove duplicated list elements
-- `rest()`: Get list without first element
-- `reverse()`: Reverse list elements
-- `setAt(pos, value)`: Set list element to value at given position
-- `setDelimiter(delimiter)`: Set list delimiter
-- `slice(start, end)`: Slice list from start to end position
-- `sort()`: Sort list
-- `swap(pos1, pos2)`: Swap list elements at given positions
-- `toArray()`: Transform list to array based on current delimiter
-
 ## License
+
 [MIT](https://github.com/larswaechter/listing.js/blob/master/LICENSE)
--- test.js
+++ test.js
@@ -1,214 +1,176 @@
-/*!
- * listing.js <https://github.com/larswaechter/listing.js>
- *
- * Copyright (c) 2017 - 2019, Lars Wächter.
- * Released under the MIT License.
- */
-
 require('mocha');
+
 const assert = require('assert');
-const listing = require('./');
+const Listing = require('./');
 
 describe('listing.js', function () {
-  it('validate list length', function () {
-    const myList = new listing('1,2,3,4');
+	it('validate list length', function () {
+		const myList = new Listing('1,2,3,4');
+		assert.equal(myList.length, 4);
+	});
 
-    assert.equal(myList.length, 4);
-  });
+	it('create new listing instance from array', function () {
+		const myList = Listing.arrayToList([1, 2, 3, 4], ',');
+		assert.equal(myList.list, '1,2,3,4');
+	});
 
-  it('create new listing instance from array', function () {
-    const myList = listing.toList([1, 2, 3, 4], ',', true);
+	it('compare list elements', function () {
+		assert.equal(new Listing('1,2,3').list, '1,2,3');
+	});
 
-    assert.equal(myList.list, '1,2,3,4');
-  });
+	it('append new element to list', function () {
+		const myList = new Listing('1,2,3');
+		myList.append(4);
+		assert.equal(myList.list, '1,2,3,4');
+	});
 
-  it('compare list elements', function () {
-    assert.equal(new listing('1,2,3').list, '1,2,3');
-  });
+	it('clear list', function () {
+		const myList = new Listing('1,2,3,4');
+		myList.clear();
+		assert.equal(myList.list, '');
+	});
 
-  it('append new element to list', function () {
-    const myList = new listing('1,2,3');
-    myList.append(4);
+	it('concat two lists into one', function () {
+		const myList = new Listing('1,2,3');
+		const myNewList = new Listing('4,5,6');
+		myList.concat(myNewList);
+		assert.equal(myList.list, '1,2,3,4,5,6');
+	});
 
-    assert.equal(myList.list, '1,2,3,4');
-  });
+	it('list element contains a certain substring', function () {
+		const myList = new Listing('hello,whats,up');
+		assert.equal(myList.contains('hat'), 1);
+	});
 
-  it('clear list', function () {
-    const myList = new listing('1,2,3,4');
-    myList.clear()
+	it('list element contains a certain substring (case-insensitive)', function () {
+		const myList = new Listing('hello,whats,up');
+		assert.equal(myList.containsNoCase('hAt'), 1);
+	});
 
-    assert.equal(myList.list, '');
-  });
+	it('delete list element', function () {
+		const myList = new Listing('1,2,3');
+		myList.deleteAt(1);
+		assert.equal(myList.list, '1,3');
+	});
 
-  it('concat two lists into one', function () {
-    const myList = new listing('1,2,3');
-    const myNewList = new listing('4,5,6');
-    myList.concat(myNewList);
+	it('filter list elements', function () {
+		const myList = new Listing('1,2,3,4,5,6');
+		const newList = myList.filter(function (val) {
+			return val % 2 === 0;
+		});
+		assert.equal(newList.list, '2,4,6');
+	});
 
-    assert.equal(myList.list, '1,2,3,4,5,6');
-  });
+	it('find list element', function () {
+		const myList = new Listing('this,is,a,test');
+		assert(myList.find('a'));
+	});
 
-  it('list element contains a certain substring', function () {
-    const myList = new listing('hello,whats,up');
+	it('find list element (case-insensitive)', function () {
+		const myList = new Listing('this,is,a,test');
+		assert(myList.findNoCase('tEsT'));
+	});
 
-    assert.equal(myList.contains('hat'), 1);
-  });
+	it('get first list element', function () {
+		const myList = new Listing('lorem,ipsum');
+		assert.equal(myList.first(), 'lorem');
+	});
 
-  it('list element contains a certain substring (case-insensitive)', function () {
-    const myList = new listing('hello,whats,up');
+	it('get list element at position', function () {
+		const myList = new Listing('check,out,my,library');
+		assert.equal(myList.getAt(2), 'my');
+	});
 
-    assert.equal(myList.containsNoCase('hAt'), 1);
-  });
+	it('get list of duplicates', function () {
+		const myList = new Listing('1,2,3,2,1,4');
+		assert.equal(myList.getDuplicates().list, '1,2');
+	});
 
-  it('delete list element', function () {
-    const myList = new listing('1,2,3');
-    myList.deleteAt(1);
+	it('get position of value', function () {
+		const myList = new Listing('1,2,3,4');
+		assert.equal(myList.indexOf('3'), 2);
+	});
 
-    assert.equal(myList.list, '1,3');
-  });
+	it('insert element at position', function () {
+		const myList = new Listing('1,2,3,5');
+		myList.insertAt(3, 4);
+		assert.equal(myList.list, '1,2,3,4,5');
+	});
 
-  it('filter list elements', function () {
-    const myList = new listing('1,2,3,4,5,6');
+	it('get last list element', function () {
+		const myList = new Listing('1,2,3');
+		assert.equal(myList.last(), '3');
+	});
 
-    const newList = myList.filter(function (val) {
-      return val % 2 === 0;
-    });
+	it('map list elements', function () {
+		const myList = new Listing('1,2,3');
+		const newList = myList.map(function (val) {
+			return val * 2;
+		});
+		assert.equal(newList.list, '2,4,6');
+	});
 
-    assert.equal(newList.list, '2,4,6');
-  });
+	it('prepend list element', function () {
+		const myList = new Listing('1,2,3');
+		myList.prepend(0);
+		assert.equal(myList.list, '0,1,2,3');
+	});
 
-  it('find list element', function () {
-    const myList = new listing('this,is,a,test');
+	it('add element at start and end of list', function () {
+		const myList = new Listing('hello,friend');
+		myList.qualify('bye');
+		assert.equal(myList.list, 'bye,hello,friend,bye');
+	});
 
-    assert(myList.find('a'));
-  });
+	it('remove duplicates', function () {
+		const myList = new Listing('1,2,3,2,4,1');
+		myList.removeDuplicates();
+		assert.equal(myList.list, '1,2,3,4');
+	});
 
-  it('find list element (case-insensitive)', function () {
-    const myList = new listing('this,is,a,test');
+	it('get list elements without first element', function () {
+		const myList = new Listing('1,2,3,4');
+		assert.equal(myList.rest(), '2,3,4');
+	});
 
-    assert(myList.findNoCase('tEsT'));
-  });
+	it('reverse list elements', function () {
+		const myList = new Listing('one,two,three,four');
+		myList.reverse();
+		assert.equal(myList.list, 'four,three,two,one');
+	});
 
-  it('get first list element', function () {
-    const myList = new listing('lorem,ipsum');
+	it('set list element at position', function () {
+		const myList = new Listing('1,2,4');
+		myList.setAt(2, 3);
+		assert.equal(myList.list, '1,2,3');
+	});
 
-    assert.equal(myList.first(), 'lorem');
-  });
+	it('set delimiter', function () {
+		const myList = new Listing('hello,how,are,you');
+		myList.setDelimiter(';');
+		assert.equal(myList.list, 'hello;how;are;you');
+	});
 
-  it('get list element at position', function () {
-    const myList = new listing('check,out,my,library');
+	it('slice list elements', function () {
+		const myList = new Listing('1,2,3,4,5');
+		assert.equal(myList.slice(1, 4), new Listing('2,3,4').list);
+	});
 
-    assert.equal(myList.getAt(2), 'my');
-  });
+	it('sort list elements', function () {
+		const myList = new Listing('4,1,2,6,3,8,9');
+		myList.sort();
+		assert.equal(myList.list, '1,2,3,4,6,8,9');
+	});
 
-  it('get list of duplicates', function () {
-    const myList = new listing('1,2,3,2,1,4');
+	it('swap list elements', function () {
+		const myList = new Listing('1,2,3,4,5');
+		myList.swap(0, 4);
+		assert.equal(myList.list, '5,2,3,4,1');
+	});
 
-    assert.equal(myList.getDuplicates().list, '1,2');
-  });
-
-  it('get position of value', function () {
-    const myList = new listing('1,2,3,4');
-
-    assert.equal(myList.indexOf('3'), 2);
-  });
-
-  it('insert element at position', function () {
-    const myList = new listing('1,2,3,5');
-    myList.insertAt(3, 4);
-
-    assert.equal(myList.list, '1,2,3,4,5');
-  });
-
-  it('get last list element', function () {
-    const myList = new listing('1,2,3');
-
-    assert.equal(myList.last(), '3');
-  });
-
-  it('map list elements', function () {
-    const myList = new listing('1,2,3');
-
-    const newList = myList.map(function (val) {
-      return val * 2;
-    });
-
-    assert.equal(newList.list, '2,4,6');
-  });
-
-  it('prepend list element', function () {
-    const myList = new listing('1,2,3');
-    myList.prepend(0);
-
-    assert.equal(myList.list, '0,1,2,3');
-  });
-
-  it('add element at start and end of list', function () {
-    const myList = new listing('hello,friend');
-    myList.qualify('bye');
-
-    assert.equal(myList.list, 'bye,hello,friend,bye');
-  });
-
-  it('remove duplicates', function () {
-    const myList = new listing('1,2,3,2,4,1');
-    myList.removeDuplicates();
-
-    assert.equal(myList.list, '1,2,3,4');
-  });
-
-  it('get list elements without first element', function () {
-    const myList = new listing('1,2,3,4');
-
-    assert.equal(myList.rest(), '2,3,4');
-  });
-
-  it('reverse list elements', function () {
-    const myList = new listing('one,two,three,four');
-    myList.reverse();
-
-    assert.equal(myList.list, 'four,three,two,one');
-  });
-
-  it('set list element at position', function () {
-    const myList = new listing('1,2,4');
-    myList.setAt(2, 3);
-
-    assert.equal(myList.list, '1,2,3');
-  });
-
-  it('set delimiter', function () {
-    const myList = new listing('hello,how,are,you');
-    myList.setDelimiter(';');
-
-    assert.equal(myList.list, 'hello;how;are;you');
-  });
-
-  it('slice list elements', function () {
-    const myList = new listing('1,2,3,4,5');
-    myList.slice(1, 4);
-
-    assert.equal(myList.list, '2,3,4');
-  });
-
-  it('sort list elements', function () {
-    const myList = new listing('4,1,2,6,3,8,9');
-    myList.sort();
-
-    assert.equal(myList.list, '1,2,3,4,6,8,9');
-  });
-
-  it('swap list elements', function () {
-    const myList = new listing('1,2,3,4,5');
-    myList.swap(0, 4);
-
-    assert.equal(myList.list, '5,2,3,4,1');
-  });
-
-  it('transform list to array', function () {
-    const myList = new listing('this,is,my,list');
-    const listArr = myList.toArray();
-
-    assert.deepEqual(listArr, ['this', 'is', 'my', 'list']);
-  });
+	it('transform list to array', function () {
+		const myList = new Listing('this,is,my,list');
+		const listArr = myList.toArray();
+		assert.deepEqual(listArr, ['this', 'is', 'my', 'list']);
+	});
 });
